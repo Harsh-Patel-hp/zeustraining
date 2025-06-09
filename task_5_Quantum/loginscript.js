2deff4c2c707a5a3657bb35fbbb09ae3f8e37948
Harsh-Patel-hp <harsh.patel@zeuslearning.com>
2025-06-09 17:06:38 +0530

    login page

---
+++ task_5_Quantum/loginscript.js
@@ -0,0 +1,8 @@
+function togglePassword() {
+  let password = document.getElementById("password");
+  if (password.type === "password") {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+}
