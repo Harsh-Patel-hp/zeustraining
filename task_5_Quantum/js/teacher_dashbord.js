45f8dfa4c84f753d8cd4250d2d6cc2c47c7ff15d
Harsh-Patel-hp <harsh.patel@zeuslearning.com>
2025-06-10 15:52:08 +0530

    dynamic func added

--- task_5_Quantum/js/teacher_dashbord.js
+++ task_5_Quantum/js/teacher_dashbord.js
@@ -1,19 +1,43 @@
 let menuBtn = document.getElementById("menu-btn");
 let navLinks = document.querySelector(".nav-links");
 let hamburger = document.querySelector(".hamburger");
+let navItems = document.querySelectorAll(".nav-links a");
 
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show");
   hamburger.classList.toggle("active");
 });
 
-document.querySelectorAll(".nav-links a").forEach((link) => {
+navItems.forEach((link) => {
   link.addEventListener("click", () => {
+    navItems.forEach((item) => item.classList.remove("active"));
+
+    link.classList.add("active");
+
     navLinks.classList.remove("show");
     hamburger.classList.remove("active");
   });
 });
 
+const courses = document.getElementById("courses");
+const classes = document.getElementById("classes");
+
+function removeActive() {
+  courses.classList.remove("courses-active");
+  classes.classList.remove("classes-active");
+}
+
+courses.addEventListener("click", function () {
+  removeActive();
+  courses.classList.add("courses-active");
+});
+
+classes.addEventListener("click", function () {
+  removeActive();
+  classes.classList.add("classes-active");
+});
+
+// Close the menu if the user clicks outside the navbar
 document.addEventListener("click", (e) => {
   if (!e.target.closest(".nav-bar")) {
     navLinks.classList.remove("show");
