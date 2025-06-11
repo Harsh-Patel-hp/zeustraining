445b47bd4c687d5252f018c32516e7582f241b44
Harsh-Patel-hp <harsh.patel@zeuslearning.com>
2025-06-11 19:01:26 +0530

    all func added

--- task_5_Quantum/js/teacher_dashbord.js
+++ task_5_Quantum/js/teacher_dashbord.js
@@ -3,7 +3,10 @@ const navBar = document.querySelector(".nav-bar");
 let navLinks = document.querySelector(".nav-links");
 let hamburger = document.querySelector(".hamburger");
 let navItems = document.querySelectorAll(".nav-links .nav-item");
-let hoverTimer;
+let alerts = document.querySelector(".alert-icon");
+let announcements = document.querySelector(".announcement-icon");
+let alertBox = document.querySelector(".alerts-box");
+let announcementBox = document.querySelector(".announcements-box");
 
 menuBtn.addEventListener("mouseover", () => {
   navLinks.classList.toggle("show");
@@ -13,6 +16,42 @@ menuBtn.addEventListener("mouseover", () => {
 menuBtn.addEventListener("mouseout", () => {
   navLinks.classList.remove("show");
   hamburger.classList.remove("white-icon");
+});
+
+alerts.addEventListener("mouseover", () => {
+  alertBox.classList.toggle("alert-box-show");
+  alerts.classList.toggle("white-icon");
+  let alertCount = alerts.querySelector(".alert-count");
+  if (alertCount) {
+    alertCount.classList.toggle("count-hide");
+  }
+});
+
+alerts.addEventListener("mouseout", () => {
+  alertBox.classList.remove("alert-box-show");
+  alerts.classList.remove("white-icon");
+  let alertCount = alerts.querySelector(".alert-count");
+  if (alertCount) {
+    alertCount.classList.remove("count-hide");
+  }
+});
+
+announcements.addEventListener("mouseover", () => {
+  announcementBox.classList.toggle("announcement-box-show");
+  let announcementCount = announcements.querySelector(".announcement-count");
+  if (announcementCount) {
+    announcementCount.classList.toggle("count-hide");
+  }
+  announcements.classList.toggle("white-icon");
+});
+
+announcements.addEventListener("mouseout", () => {
+  announcementBox.classList.remove("announcement-box-show");
+  let announcementCount = announcements.querySelector(".announcement-count");
+  if (announcementCount) {
+    announcementCount.classList.remove("count-hide");
+  }
+  announcements.classList.remove("white-icon");
 });
 
 navItems.forEach((link) => {
